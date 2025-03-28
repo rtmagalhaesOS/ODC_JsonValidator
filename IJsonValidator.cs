ca1e880a89fe2fc002fc309fa90bed242d2e1d97
rtmagalhaesOS <ricardo.magalhaes@outsystems.com>
2025-03-28 17:46:31 +0000

    Add project files.

---
+++ IJsonValidator.cs
@@ -0,0 +1,22 @@
+﻿using OutSystems.ExternalLibraries.SDK;
+
+namespace JsonValidator
+{
+    [OSInterface(Name = "JsonValidator", Description = "Validates a given JSON file against a specified JSON schema.", IconResourceName = "JsonValidator.resources.json.png")]
+    public interface IJsonValidator
+    {
+        /// <summary>
+        /// Validates a given JSON file against a specified JSON schema.
+        /// </summary>
+        /// <param name="jsonFile">The content of the JSON file to be validated, represented as a byte array.</param>
+        /// <param name="jsonSchema">The JSON schema to validate the JSON file against, represented as a byte array.</param>
+        /// <param name="licenseKey">The license key required for validating the JSON file, if applicable.</param>
+        /// <returns>A string indicating the validation result, such as success or error details.</returns>
+        [OSAction(Description = "Validates a JSON file against a specified JSON schema.")]
+        public void JsonValidate(
+            [OSParameter(Description = "The content of the JSON file to be validated, represented as a byte array.")] byte[] jsonFile,
+            [OSParameter(Description = "The JSON schema to validate the file against, represented as a byte array.")] byte[] jsonSchema,
+            [OSParameter(Description = "The license key required for validation.")] string licenseKey,
+            [OSParameter(Description = "A string that will contain validation errors, if any occur during the validation process.")] out string errors);
+    }
+}
---
+++ JsonValidator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using System.Text;
+using System;
+
+namespace JsonValidator
+{
+    public class JsonValidator : IJsonValidator
+    {
+        public void JsonValidate(byte[] jsonFile, byte[] jsonSchema, string licenseKey, out string errors)
+        {
+            if (!string.IsNullOrEmpty(licenseKey))
+            {
+                // replace with your license key
+                License.RegisterLicense(licenseKey);
+            }
+
+            /*
+            string jsonSchemaString;
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream("JsonValidator.schema.json"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                jsonSchemaString = reader.ReadToEnd();
+            }*/
+
+            // Read the JSON Schema and JSON Document 
+            string jsonSchemaString = Encoding.UTF8.GetString(jsonSchema);
+            string jsonDocumentString = Encoding.UTF8.GetString(jsonFile);
+
+            errors = ValidateJson(jsonSchemaString, jsonDocumentString);
+        }
+
+        static string ValidateJson(string jsonSchemaString, string jsonDocumentString)
+        {
+            string output = "";
+            try
+            {
+                JSchema schema = JSchema.Parse(jsonSchemaString);
+                JObject jsonDocument = JObject.Parse(jsonDocumentString);
+
+                if (!jsonDocument.IsValid(schema, out IList<ValidationError> errors))
+                {
+                    foreach (var error in errors)
+                    {
+                        if (error.LineNumber > 0 && error.LinePosition > 0)
+                        {
+                            output += $"- {error.Message} Path: {error.Path} Line: {error.LineNumber}, Position: {error.LinePosition}";
+                        }
+                    }
+                }
+                return output;
+
+            }
+            catch (Exception ex)
+            {
+                output += $"- {ex.Message}";
+                return output;
+            }
+        }
+    }
+}
